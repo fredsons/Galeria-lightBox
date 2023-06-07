49e91ff9c4d8e01ff64d1983dce02dfdb673f74a
Fredson <fredsondev@gmail.com>
2023-06-07 17:08:20 -0300

    comentarios no js

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+// Selecionar os elementos da galeria e do lightbox
 const galleryItens = document.querySelectorAll('.gallery-item');
-const lightbox = document.querySelector(".lightbox")
-const lightboxImage = document.querySelector(".lightbox-image")
-const lightboxClose = document.querySelector(".lightbox-close")
+const lightbox = document.querySelector(".lightbox");
+const lightboxImage = document.querySelector(".lightbox-image");
+const lightboxClose = document.querySelector(".lightbox-close");
 
+// Adicionar um evento de clique a cada item da galeria
 galleryItens.forEach((item) => {
   item.addEventListener('click', () => {
+    // Ativar o lightbox ao clicar em um item da galeria
     lightbox.classList.add('active');
-    const imageUrl = item.querySelector('.gallery-image').getAttribute('data-src')
+
+    // Obter a URL da imagem do item da galeria
+    const imageUrl = item.querySelector('.gallery-image').getAttribute('data-src');
+
+    // Atualizar a imagem exibida no lightbox com a URL obtida
     lightboxImage.setAttribute('src', imageUrl);
-    lightbox.style.display = "flex"
 
-  })
-})
+    // Exibir o lightbox
+    lightbox.style.display = "flex";
+  });
+});
 
+// Adicionar um evento de clique ao botão de fechar do lightbox
 lightboxClose.addEventListener('click', () => {
+  // Desativar o lightbox ao clicar no botão de fechar
   lightbox.classList.remove('active');
-  lightbox.style.display = "none"
-})
+
+  // Ocultar o lightbox
+  lightbox.style.display = "none";
+});
